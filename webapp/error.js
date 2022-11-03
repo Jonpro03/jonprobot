c1dc05f41ece362930d4d312a5b3d94bffc36eb9
Jon Pro <jonpro03@gmail.com>
2022-11-02 21:10:16 -0500

    Catching up

---
+++ webapp/error.js
@@ -0,0 +1,86 @@
+let handleError = function (msg, url, lineNo, columnNo, error) {
+  let messageText = `${msg} - ${url} ${lineNo}/${columnNo} ${error}`;
+  let errorModalHtml = `
+    <div id="errorModal" class="modal fade" tabindex="-1">
+      <div class="modal-dialog modal-lg modal-fullscreen-md-down text-dark fs-4" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h2 id="errorModalTitle" class="modal-title">ERROR</h2>
+          </div>
+          <div id="errorModalBody" class="modal-body">
+            <p>
+                Bad things have happened.
+                </br>
+                <a href="https://www.reddit.com/user/jonpro03/submitted/" target="_blank">Check here to see if it's a known issue.</a>
+                </br>
+                Otherwise, please please please send the report to me so that I can fix it.
+            </p>
+            </br>
+            <blockquote class="blockquote">
+                <strong id="errorText">
+                    ${messageText}
+                </strong>
+            </blockquote>
+          </div>
+          <div class="modal-footer">
+            <a href="https://www.reddit.com/message/compose/?to=jonpro03&subject=Computershared%20Crash%20Report&message=${encodeURIComponent(messageText)}" target="_blank">
+                <button class="btn btn-success">SEND REPORT</button>
+            </a>
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">CLOSE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  let d = document.createElement('div');
+  d.innerHTML = errorModalHtml;
+  document.getElementById('pageContent').appendChild(d);
+  let modal = new bootstrap.Modal(document.getElementById('errorModal'));
+  modal.show();
+  return false;
+}
+
+let handleRejection = function (event) {
+  let messageText = event.reason;
+  let errorModalHtml = `
+    <div id="errorModal" class="modal fade" tabindex="-1">
+      <div class="modal-dialog modal-lg modal-fullscreen-md-down text-dark fs-4" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h2 id="errorModalTitle" class="modal-title">ERROR</h2>
+          </div>
+          <div id="errorModalBody" class="modal-body">
+            <p>
+              Bad things have happened.
+              </br>
+              <a href="https://www.reddit.com/user/jonpro03/submitted/" target="_blank">Check here to see if it's a known issue.</a>
+              </br>
+              Otherwise, please please please send the report to me so that I can fix it.
+            </p>
+            </br>
+            <blockquote class="blockquote">
+                <strong id="errorText">
+                    ${messageText}
+                </strong>
+            </blockquote>
+          </div>
+          <div class="modal-footer">
+            <a href="https://www.reddit.com/message/compose/?to=jonpro03&subject=Computershared%20Crash%20Report&message=${encodeURIComponent(messageText)}" target="_blank">
+                <button class="btn btn-success">SEND REPORT</button>
+            </a>
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">CLOSE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `;
+  let d = document.createElement('div');
+  d.innerHTML = errorModalHtml;
+  document.getElementById('pageContent').appendChild(d);
+  let modal = new bootstrap.Modal(document.getElementById('errorModal'));
+  modal.show();
+  return false;
+}
+
+window.onerror = handleError;
+window.onunhandledrejection = handleRejection;
--- webapp/learn.js
+++ webapp/learn.js
@@ -7,21 +7,24 @@ showHelpModal = function (articleName) {
       helpModalTitle.innerHTML = "DRS Estimate Line Chart";
       helpModalBody.innerHTML = `<p>
       This chart shows how many shares are estimated to have been direct-registered over time.
-      <img src="site-images/estimates.png?a=201" class="card-img-top" alt="estimates chart">
+      <img src="site-images/estimates.png?a=204" class="card-img-top" alt="estimates chart">
       <strong>X-Axis:</strong> Date
       </br>
       <strong>Y-Axis:</strong> Share Count
       </p>
       <p>
       By default, estimates using the Sampled Data 
-      <a target="_blank" href="https://en.wikipedia.org/wiki/Arithmetic_mean">Average</a> (Purple), 
-      <a target="_blank" href="https://en.wikipedia.org/wiki/Truncated_mean">Trimmed Average</a> (Cyan) 
-      and <a target="_blank" href="https://en.wikipedia.org/wiki/Average#Median">Median</a> (Yellow) are shown, 
+      <a target="_blank" href="https://en.wikipedia.org/wiki/Arithmetic_mean">Average</a> (Purple),  
       as well as actual progress as reported by Gamestop (White).
       </br>
       Not shown is Sampled Data 
       <a target="_blank" href="https://en.wikipedia.org/wiki/Average#Mode">Mode</a> (Dark Cyan), 
+      <a target="_blank" href="https://en.wikipedia.org/wiki/Truncated_mean">Trimmed Average</a> (Cyan) 
+      and <a target="_blank" href="https://en.wikipedia.org/wiki/Average#Median">Median</a> (Yellow) are shown,
       but can be enabled by clicking it in the top legend.
+      <div class="ratio ratio-4x3">
+        <video autoplay playsinline webkit-playsinline loop class="embed-responsive-item" src="site-images/estimates.mp4?a=204" type="video/mp4"></video>
+      </div>
       </p>
       <p>
       This chart is zoomed in to fit your device's screen. Zoom with mousewheel or pinching. Scroll by dragging left or right.
@@ -34,7 +37,7 @@ showHelpModal = function (articleName) {
       helpModalBody.innerHTML = `<p>
       This chart illustrates data collected from the Reddit Sample Set. Keep in mind that this only shows
       data that has been sampled from Reddit, and does not represent all Computershare Accounts.
-      <img src="site-images/shares.png?a=201" class="card-img-top" alt="shares chart">
+      <img src="site-images/shares.png?a=204" class="card-img-top" alt="shares chart">
       <strong>X-Axis:</strong> Date
       </br>
       <strong>Left Y-Axis:</strong> Cumulative Share Count
@@ -49,7 +52,7 @@ showHelpModal = function (articleName) {
       The Yellow Lines (stacked on top) illustrate the number of Shares that have been added to existing Computershare Accounts.
       </br>
       These data points can be viewed individually by toggling them in the chart's top legend.
-      <img src="site-images/shares2.png?a=201" class="card-img-top" alt="shares chart">
+      <img src="site-images/shares2.png?a=204" class="card-img-top" alt="shares chart">
       </p>
       <p>
       This chart is zoomed in to fit your device's screen. Zoom with mousewheel or pinching. Scroll by dragging left or right.
@@ -62,7 +65,7 @@ showHelpModal = function (articleName) {
       helpModalBody.innerHTML = `<p>
       This chart illustrates data collected from the Reddit Sample Set. Keep in mind that this only shows
       data that has been sampled from Reddit, and does not represent all Computershare Accounts.
-      <img src="site-images/highscores.png?a=201" class="card-img-top" alt="highscores chart">
+      <img src="site-images/highscores.png?a=204" class="card-img-top" alt="highscores chart">
       <strong>X-Axis:</strong> Date
       </br>
       <strong>Y-Axis:</strong> Number of Accounts Derived from Account Number
@@ -84,7 +87,7 @@ showHelpModal = function (articleName) {
       helpModalBody.innerHTML = `<p>
       This chart shows various statistics of the Reddit Sample Set over time.
       
-      <img src="site-images/stats.png?a=201" class="card-img-top" alt="statistics chart">
+      <img src="site-images/stats.png?a=204" class="card-img-top" alt="statistics chart">
       <strong>X-Axis:</strong> Date
       </br>
       <strong>Y-Axis:</strong> Share Count
@@ -122,7 +125,7 @@ showHelpModal = function (articleName) {
       <a target="_blank" href="https://en.wikipedia.org/wiki/Histogram">histogram</a> 
       of individual account balances over a 
       <a target="_blank" href="https://en.wikipedia.org/wiki/Geometric_progression">geometric progression</a> distribution.
-      <img src="site-images/distribution.png?a=201" class="card-img-top" alt="histogram chart">
+      <img src="site-images/distribution.png?a=204" class="card-img-top" alt="histogram chart">
       <strong>X-Axis:</strong> Number of Shares
       </br>
       <strong>Y-Axis:</strong> Number of Accounts
@@ -133,8 +136,8 @@ showHelpModal = function (articleName) {
       </p>
       <p>
       <h3>Example</h3>
-      <p>In the following image, there are 2179 accounts with at least 128 shares, but fewer than 256 shares.
-      <img src="site-images/distribution2.png?a=201" class="card-img-top" alt="histogram chart">
+      <p>In the following image, there are 2205 accounts with at least 128 shares, but fewer than 256 shares.
+      <img src="site-images/distribution2.png?a=204" class="card-img-top" alt="histogram chart">
       </p>
       `;
       break;
@@ -143,7 +146,7 @@ showHelpModal = function (articleName) {
       helpModalTitle.innerHTML = "Account Growth Line Chart";
       helpModalBody.innerHTML = `<p>
       This chart plots how existing (sampled) Computershare Accounts have changed over time.
-      <img src="site-images/growth.png?a=201" class="card-img-top" alt="growth chart">
+      <img src="site-images/growth.png?a=204" class="card-img-top" alt="growth chart">
       <strong>X-Axis:</strong> Week Beginning Date
       </br>
       <strong>Y-Axis:</strong> Percent Accounts Grew
@@ -159,7 +162,7 @@ showHelpModal = function (articleName) {
       helpModalTitle.innerHTML = "Purchase Power USD/Week Line Chart";
       helpModalBody.innerHTML = `<p>
       This chart estimates the ability of the sample to register new shares.
-      <img src="site-images/power.png?a=201" class="card-img-top" alt="purchase power chart">
+      <img src="site-images/power.png?a=204" class="card-img-top" alt="purchase power chart">
       <strong>X-Axis:</strong> Week Beginning Date
       </br>
       <strong>Y-Axis:</strong> Median Purchasing Power in USD/week.
@@ -171,7 +174,7 @@ showHelpModal = function (articleName) {
       <strong>Example:</strong> If an account purchased $1000 worth of stock, and after 4 weeks
       have made no additional purchases, the purchase power is $250/week. After 5 weeks, it becomes $200/week.
       </br>
-      The cost basis of Computershare Accounts is largely unknown, so results are calculated with a proprietary
+      The cost basis of Computershare Accounts is largely unknown, so results are calculated with a arbitrary
       cost basis of $37.50.
       </p>
       <p>
@@ -186,16 +189,118 @@ showHelpModal = function (articleName) {
       `;
       break;
 
+    case "calculator":
+      helpModalTitle.innerHTML = "Calculator and Shares Donut";
+      helpModalBody.innerHTML = `<p>
+      Fundamentally, computershared.net is a calculator that can be used to estimate retail's progress in direct-registering Gamestop shares with the company's registrar, Computershare.
+      </p>
+      <h3>Estimating</h3>
+      <p>
+      Every 15 minutes, Computershared.net's Reddit Scraper downloads every post from a variety of subreddits. It uses computervision to extract the text from the image,
+      identifies images that are screenshots of Computershare portfolios, and extracts the number of shares in that account.
+      </br>
+      Only accounts that have been updated in the last 180 days are used by computershared.net, collectively called the <strong>Sample Set</strong>.
+      </br>
+      </p>
+      <p>
+      Additionally, the scraper looks for computershare account numbers, which are incremental and can be multiplied with metrics of the sample set
+      to estimate total shares registered, as illustrated in the <strong>DRS Estimation</strong> section:
+      <div class="ratio ratio-4x3">
+        <video autoplay playsinline webkit-playsinline loop class="embed-responsive-item" src="site-images/metric.mp4?a=204" type="video/mp4"></video>
+      </div>
+      Computershared.net enables you to estimate the total using <strong>Average</strong> (Mean), <strong>Median</strong>, <strong>Mode</strong>, and <strong>Trimmed Average</strong>.
+      </br>
+      <strong>Trimmed Average</strong> is the default and most accurate metric. It is fitted to follow Actual DRS Progress released by Gamestop.
+      </p>
+      </br>
+      <h3>Share Allocation, Progress and Completion</h3>
+      <p>
+      <strong>Computershared.net and its author no claims and takes no stance on which allocations, or how many of the issued share balance need to be direct-registered to affect any outcome nor what those outcomes may be.</strong>
+      There is no reliable precendence, and there is no community concensus on the matter (and honestly ya'll, it's not really the point of computershared.net).
+      </br>
+      As such, the site was designed from the very beginning to be very flexible and suit a variety of opinions.
+      <p>
+      In the <strong>Share Ownership</strong> section, the issued shares are broken down by known ownership allocations (see cited source). Each allocation shows both number of shares
+      and percent of the total issued shares.
+      </br>
+      These ownership allocations can be toggled to customize the progress calculation, and share allocation donut.
+      The following example illustrates how to setup the site for one who believes that all shares except ones owned by Insiders need to be direct registered:
+      <div class="ratio ratio-4x3">
+        <video autoplay playsinline webkit-playsinline loop class="embed-responsive-item" src="site-images/calculator.mp4?a=204" type="video/mp4"></video>
+      </div>
+      As a user of the site, you can select any combination of allocations you desire.
+      </p>
+      <p>
+      The donut can also be used to compare allocations to each other. The following example can be used to compare retail to insiders.
+      <div class="ratio ratio-4x3">
+        <video autoplay playsinline webkit-playsinline loop class="embed-responsive-item" src="site-images/calculator3.mp4?a=204" type="video/mp4"></video>
+      </div>
+      </p>
+      <p>
+      Another useful comparison is retail to remaining...
+      <div class="ratio ratio-4x3">
+        <video autoplay playsinline webkit-playsinline loop class="embed-responsive-item" src="site-images/calculator2.mp4?a=204" type="video/mp4"></video>
+      </div>
+      </p>
+      `;
+      break;
+
+    case "metrics":
+      helpModalTitle.innerText = "Sample Metrics";
+      helpModalBody.innerHTML = `
+      <strong>Average:</strong> Sum of all account balances, divided by the number of accounts.
+      </br>
+      <strong>Median:</strong> Value at which half of all accounts have a higher balance, and half of all accounts have a lower.
+      </br>
+      <strong>Mode:</strong> Of all account balances, this value is the most common.
+      </br>
+      <strong>Trimmed Average:</strong> Same as <strong>Average</strong>, except that the largest 4% of accounts and the smallest 4% of accounts are excluded from the average.
+      </br>
+      `;
+      break;
+
+    case "posts":
+      helpModalTitle.innerText = "Reddit Activity";
+      helpModalBody.innerHTML = `<p>
+      This chart illustrates DRS image activity on Reddit over time.
+      
+      <img src="site-images/posts.png?a=204" class="card-img-top" alt="statistics chart">
+      <strong>X-Axis:</strong> Date
+      </br>
+      <strong>Left Y-Axis:</strong> Number of Shareholders on Reddit
+      </br>
+      <strong>Right Y-Axis:</strong> Number of Posts on Reddit
+      </p>
+      <p>
+      In Blue, the daily count of posts identified as Computershare portfolio images are displayed as a bar graph.
+      </br>
+      In Pink, the total number of Active Shareholders is shown as a line graph. An Active Shareholder is one who has
+      posted a screenshot of their Computershare Portfolio to Reddit in the last 180 days.
+      </br>
+      In Red, the total number of Stale Shareholders is shown as a line graph. A Stale Shareholder is one who has
+      <strong>NOT</strong> posted a screenshot of their Computershare Portfolio to Reddit in the last 180 days.
+      </br>
+      <a href="./faqs/#2.5">Learn More</a>.
+      </p>
+      <p>
+      This chart is zoomed in to fit your device's screen. Zoom with mousewheel or pinching. Scroll by dragging left or right.
+      </p>
+      `;
+      break;
+
   }
 
   var modal = new bootstrap.Modal(document.getElementById('helpModal'));
   modal.show();
 }
 
-document.getElementById('learnEstimates').onclick = () => { showHelpModal("estimates") };
-document.getElementById('learnShares').onclick = () => { showHelpModal("shares") };
-document.getElementById('learnHighscore').onclick = () => { showHelpModal("highscores") };
-document.getElementById('learnStats').onclick = () => { showHelpModal("statistics") };
-document.getElementById('learnDistribution').onclick = () => { showHelpModal("distribution") };
-document.getElementById('learnGrowth').onclick = () => { showHelpModal("growth") };
-document.getElementById('learnPower').onclick = () => { showHelpModal("power") };
+document.getElementById('learnCalc').onclick = () => { showHelpModal("calculator") };
+document.getElementById('learnMetrics').onclick = () => { showHelpModal("metrics") };
+document.getElementById('learnDataset').onclick = () => { 
+  new bootstrap.Modal(document.getElementById('stats')).show();
+ };
+
+ document.getElementById('learnChart').addEventListener('click', () => {
+  var chart = document.getElementById('chartType').innerText;
+  showHelpModal(chart);
+ });
